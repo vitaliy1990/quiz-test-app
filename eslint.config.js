fd3b74ed0591802f69fbc831e5628e19f2afa645
vitaliy1990 <Chumakov_vitaliy@meta.ua>
2025-02-27 23:54:53 +0200

    add prettier and eslint

---
+++ eslint.config.js
@@ -0,0 +1,98 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+// PLUGINS //
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import prettierPlugin from 'eslint-plugin-prettier';
+import react from 'eslint-plugin-react';
+import unusedImports from 'eslint-plugin-unused-imports';
+import eslintPluginImport from 'eslint-plugin-import';
+
+export default tseslint.config(
+  { ignores: ['dist'] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      react,
+      'unused-imports': unusedImports,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      import: eslintPluginImport,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'prettier/prettier': 'error',
+      'react/jsx-max-props-per-line': ['error', { maximum: 1 }],
+      'react/jsx-first-prop-new-line': ['error', 'multiline'],
+      '@typescript-eslint/no-unused-vars': 'off',
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'error',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
+      'import/order': [
+        'error',
+        {
+          groups: [
+            ['builtin', 'external'],
+            ['internal'],
+            ['parent', 'sibling', 'index'],
+          ],
+          pathGroups: [
+            {
+              pattern: 'react',
+              group: 'builtin',
+              position: 'before',
+            },
+            {
+              pattern: 'components',
+              group: 'internal',
+            },
+            {
+              pattern: 'common',
+              group: 'internal',
+            },
+            {
+              pattern: 'routes/ **',
+              group: 'internal',
+            },
+            {
+              pattern: 'assets/**',
+              group: 'internal',
+              position: 'after',
+            },
+          ],
+          pathGroupsExcludedImportTypes: ['internal'],
+          'newlines-between': 'always',
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+            orderImportKind: 'asc',
+          },
+        },
+      ],
+      'import/no-unresolved': ['error', { commonjs: true, amd: true }],
+      'import/named': 'off',
+      'import/namespace': 'error',
+      'import/default': 'error',
+      'import/export': 'error',
+    },
+  }
+);
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+import './App.css';
+
+const App: FC = () => {
+  return <div className='bg-amber-50 p-7 text-2xl text-amber-950'>HELLO</div>;
+};
+
+export default App;
